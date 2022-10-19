52685a505df0999281eb09c5c5f6051317c36d40
Dmitriy S <sviridenko_da@smart-transport.ru>
2022-10-20 05:44:30 +0600

    update settings for autograph client

--- garbage/configuration/settings.py
+++ garbage/configuration/settings.py
@@ -9,10 +9,10 @@ class Settings(BaseSettings):
     OSRM_DEFAULT_REQUEST_TIMEOUT: int = 30
     OSRM_BASE_URL: AnyUrl = 'http://127.0.0.1:5000/'
 
-    EGTS_DEFAULT_REQUEST_TIMEOUT: int = 30
-    EGTS_BASE_URL: AnyUrl = 'http://127.0.0.1/ServiceJSON/'
-    EGTS_SERVICE_LOGIN: str = ''
-    EGTS_SERVICE_PASSWORD: str = ''
+    AUTOGRAPH_DEFAULT_REQUEST_TIMEOUT: int = 30
+    AUTOGRAPH_BASE_URL: AnyUrl = 'https://127.0.0.1/ServiceJSON/'
+    AUTOGRAPH_SERVICE_LOGIN: str = ''
+    AUTOGRAPH_SERVICE_PASSWORD: str = ''
 
     class Config:
         env_file = '.environment'
